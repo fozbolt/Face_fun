00abd5f8923502cd3c7e6df38ad077a744ff8fb0
fozbolt <58041469+fozbolt@users.noreply.github.com>
2021-01-30 19:48:48 +0100

    Added code for camera API

--- app/src/main/java/com/example/facefun/MainActivity.java
+++ app/src/main/java/com/example/facefun/MainActivity.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView myImageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 101; //nezz zasto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Face fun"); //za main activity
+
+        myImageView = findViewById(R.id.button_gallery);
+
+        // https://stackoverflow.com/questions/6016000/how-to-open-phones-gallery-through-code
+
     }
 
     public void launchPhotoUpload(View v){
